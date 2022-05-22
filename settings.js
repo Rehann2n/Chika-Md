cb656c4a2bc5b174993c457682169dc676c35888
Rehann2n <106019341+Rehann2n@users.noreply.github.com>
2022-05-22 22:12:19 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,22 +22,21 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6285824073406','6285824073406','6285824073406'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6285824073406'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6285824073406' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Rehann' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Ch bot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = '2n' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+global.ig = 'https://instagram.com/rhnn04__'
+global.mygc = 'https://chat.whatsapp.com/LXvmHCSlMd97WP6m6BpT8t'
 global.myweb = 'https://s.id/RiyAPIs'
-global.email = 'riychdwaynee@gmail.com'
+global.email = 'muhraihan01@sma.belajar.id'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
